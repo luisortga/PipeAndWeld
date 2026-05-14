63fe03015f059e417fab8afd995ee0750df09759
luis Ortega <ortegaluiskb@gmail.com>
2026-05-14 15:52:11 -0600

    se agrega alloy e incolloy

---
+++ src/alloy.py
@@ -0,0 +1,10 @@
+# Diccionario de medidas en aceros alloy
+
+alloy = {
+    "1/2": 0.84,
+    "3/4": 0.84,
+    "1": 0.84,
+    "1 1/4": 0.84,
+    "1 1/2": 0.84
+}
+
---
+++ src/incolloy.py
@@ -0,0 +1,10 @@
+# Dicconario de medidas en aceros incolloy
+
+incolloy = {
+    "1/2": 0.84,
+    "3/4": 0.84,
+    "1": 0.84,
+    "1 1/4": 0.84,
+    "1 1/2": 0.84
+}
+
