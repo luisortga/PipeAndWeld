7cd33eda1da1fb7bc6bf9764f76a47c526e19be8
luis Ortega <ortegaluiskb@gmail.com>
2026-05-29 11:32:42 -0600

    mas desarrollo en deply

--- src/funciones_order.py
+++ src/funciones_order.py
@@ -1,7 +1,20 @@
 """
 @author: luisorteg
 """
+from typing import Callable
 
 current: int = 0
+
 # lambda all in line single
-sum_two_value = lambda f_value, s_value: f_value+s_value
+sum_two_value: Callable[[int, int], int] = lambda f_value, s_value: f_value+s_value
+
+print(sum_two_value(12, 36))
+
+# mejor practica funcion estandar de una sola linea
+
+def sum_two(firts_v: int, second_v: int) -> int: return firts_v+second_v
+
+
+def multiply(firts_v: float, second_v: float) -> float: return firts_v**second_v
+
+print(sum_two(12, 16))
