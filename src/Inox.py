ac84233ab4b2b1a8b89bc0b55aa6a81dd02c5977
luis Ortega <ortegaluiskb@gmail.com>
2026-06-17 12:31:52 -0600

    Update Inox with threads

--- src/Inox.py
+++ src/Inox.py
@@ -7,3 +7,19 @@ def comparision() -> None:
     pass
 
 # pendiente en implementar
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    # logging.getLogger().setLevel(logging.DEBUG)
+
+    pipeline = Pipeline()
+    event = threading.Event()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(producer, pipeline, event)
+        executor.submit(consumer, pipeline, event)
+
+        time.sleep(0.1)
+        logging.info("Main: about to set event")
+        event.set()
