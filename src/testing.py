72228709d7a351bcc97644dc51999872425d09f0
luis Ortega <ortegaluiskb@gmail.com>
2026-06-17 21:01:23 -0600

    Update conjunto set

--- src/testing.py
+++ src/testing.py
@@ -8,3 +8,8 @@ iteration: tuple[int, int] = (0, 24)
 for i in iteration: print(f'salida desde una tuple')
 
 new_tuple: tuple[int, int] = (12, 16)
+
+# continue
+
+# set
+conjunto_time = {12, 16, 4, 48}
