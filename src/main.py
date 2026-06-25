e4ceb2c63e38adfa5a5ace57f742369c4a5c7a53
luisOrtga <ortegaluiskb@gmail.com>
2026-06-24 22:46:31 -0600

    Upgrade full pipe and weld

--- src/main.py
+++ src/main.py
@@ -7,15 +7,15 @@
 def include(point_x: int, point_y: int):
     if point_y < 16:
         altura = "Recuerda que la altura maxima es 16"
-        return  print(len(point_x), altura)
+        return  print(len(point_x), altura) # type: ignore
     else:
         return print(f"dejamos los datos siguiente: {point_x} and {point_y}")
-    
+
 
 class advancelbow:
 
     # Create the 90 degree long radius elbow class
-    def elbow90(milimeter,inch):            
+    def elbow90(self, milimeter: float,inch: float):            
             # Converter milimeter in inches
             result_inch = milimeter/25.4
             radio = result_inch/2
