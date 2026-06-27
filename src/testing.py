0c4737a8e082515e41e37b807fdfc12e4f7a810a
luisOrtga <ortegaluiskb@gmail.com>
2026-06-26 20:15:47 -0600

    Upgrade testing with request

--- src/testing.py
+++ src/testing.py
@@ -1,15 +1,6 @@
+#
 import time
+import requests
 
-for i in range(0, 16): print(f'INOX. {i}')
-time.sleep(12)
-
-iteration: tuple[int, int] = (0, 24)
-
-for i in iteration: print(f'salida desde una tuple')
-
-new_tuple: tuple[int, int] = (12, 16)
-
-# continue
-
-# set
-conjunto_time = {12, 16, 4, 48}
+def check_api():
+    pass
