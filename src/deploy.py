c7671e61f01727f8777b9cb4292bbf5e0d3d286c
luisOrtga <ortegaluiskb@gmail.com>
2026-06-26 20:08:45 -0600

    Upgrade deploy in running

--- src/deploy.py
+++ src/deploy.py
@@ -1,5 +1,24 @@
-# build
+# Generadores
+# remember 12_000_000
+# Generator
 
-SOUL = 'luis'
+"""
+    Generadores
+"""
 
-cantidad: int = 10_000
+gen_cuadrador = (x**2 for x in range(8))
+print(next(gen_cuadrador))
+
+for x in gen_cuadrador:
+    print(x)
+
+def contar():
+    yield 1
+    yield 2
+    yield 3
+
+g = contar()
+
+print(next(g))
+print(next(g))
+print(next(g))
