86b28445cc4af5171c7d08608e2599b074122f9f
luisOrtga <ortegaluiskb@gmail.com>
2026-06-26 20:20:32 -0600

    Update testing with request

--- src/testing.py
+++ src/testing.py
@@ -2,5 +2,15 @@
 import time
 import requests
 
-def check_api():
-    pass
+def check_api(api: str):
+    time.sleep(2)
+    try:
+        response = requests.get(api)
+        if response.status_code == 200:
+            print(f'Success: {api} esta funcionando perfectamente')
+        else:
+            print(f'Error: {api} respondio con el código de estado {response.status_code}.')
+    except requests.RequestException:
+        print(f'Error: {api} esta caido.')
+        
+
