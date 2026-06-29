10308445de4b6e2796799e16e8798a562f9d93f7
luisOrtga <ortegaluiskb@gmail.com>
2026-06-28 22:44:49 -0600

    Add code document in incolloy

--- src/incolloy.py
+++ src/incolloy.py
@@ -8,3 +8,53 @@ incolloy = {
     "1 1/2": 0.84
 }
 
+#
+
+class Three:
+    
+    """
+        Class developed exclusively for github
+    """
+
+    def clousure(self, **kwargs: str) -> None:
+        """
+            test data base of snickers
+        """
+        import time
+        try:
+            if "under armour" in kwargs.values():
+                print('next fase ->')
+                for i in range(0, 16):
+                    time.sleep(2)
+                    print('process...')
+                    if i == 15:
+                        print('sucessful')
+            elif 'undefined' not in kwargs.values():
+                count: int = 0
+                while True:
+                    count += 1
+                    time.sleep(2)
+                    print(f'{count*25}') # 25, 50, 75 and 100
+                    if count == 4:
+                        print('listo')
+                        break
+            else:
+                raise TypeError('Error de interprete')
+        except ValueError as e:
+            print(f'Error: {e}')
+        # return none
+        
+
+if __name__ == "__main__":
+    first_three = Three()
+    print(first_three)
+    
+    # dict
+    snickers: dict[str, str] = {
+    "basketball": "under armour",
+    "running": "nike",
+    "casual": "adidas",
+    }
+    
+    first_three.clousure(**snickers) # desempaquetado
+
