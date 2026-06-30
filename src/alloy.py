197df5b6d79b65b44a3c0fca2a17397ae3865d72
luisOrtga <ortegaluiskb@gmail.com>
2026-06-29 23:17:33 -0600

    Update alloy with dict

--- src/alloy.py
+++ src/alloy.py
@@ -18,3 +18,67 @@ inox = {
     'rust': 16,
     'ruby': 18
 }
+
+# manipular datos
+
+class Meta:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def empaquetado(self, biblioteca: dict[str, str | int]):
+        """
+            datos empaquetados.
+        """
+        
+        total: int = sum(valor for valor in biblioteca.values() if isinstance(valor, int))
+        print(total)
+        for valor in biblioteca.values():
+            if isinstance(valor, str):
+                print(f'mark {valor}')
+            else:
+                print(f"num {valor}")
+    
+    def desempaquetado(self, **kwargs: str):
+        total: int = 0
+        for k, v in kwargs.items():
+            if isinstance(v, int):
+                print(k)
+                total += v
+                print(f'El total va {total}')
+            else:
+                print(f'key: {k} and value {v}')
+    
+    def tensores_test(self, tensores: dict[str, dict[str, str | int] | tuple[str, ...]]):
+        pass
+    
+class Data:
+    
+    def __init__(self,) -> None:
+        pass
+    
+    def bibliotecas(self) -> dict[str, str | int]:
+        return {
+            'basketball': 'nike air',
+            'running': 'under armour',
+            'casual': 'adidas',
+            'extreme': 'caterpiller',
+            'court': 16,
+            'tennis': 28
+        }
+        
+    def tensor(self) -> dict[str, dict[str, str | int]]:
+        return {
+            
+        }
+        
+if __name__ == "__main__":
+    
+    datos: Data = Data()
+    logic = Meta()
+    
+    database: dict[str, str | int] = datos.bibliotecas()
+    
+    logic.empaquetado(database)
+    
+    # desempaquetado
